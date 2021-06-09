df27444e9d4df85d6222f6a22987e26334583024
Jose Badeau <jose.badeau@gmail.com>
2021-06-09 10:11:03 +0200

    avero to json

--- modules/jira-oslc/src/main/java/com/sixgroup/sixsense/jira/oslc/JiraIssueToOslcChangeRequest.java
+++ modules/jira-oslc/src/main/java/com/sixgroup/sixsense/jira/oslc/JiraIssueToOslcChangeRequest.java
@@ -4,6 +4,13 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import static za.co.absa.abris.avro.functions.from_avro;
+
+import static org.apache.spark.sql.functions.col;
+
+import za.co.absa.abris.config.AbrisConfig;
+import za.co.absa.abris.config.FromAvroConfig;
+
 public class JiraIssueToOslcChangeRequest {
 
     public static void main(String[] args) {
@@ -14,6 +21,12 @@ public class JiraIssueToOslcChangeRequest {
         spark.sparkContext()
                 .setLogLevel("ERROR");
 
+        FromAvroConfig fromAvroConfig = AbrisConfig
+                .fromConfluentAvro()
+                .downloadReaderSchemaByLatestVersion()
+                .andTopicNameStrategy(System.getProperty("KAFKA_TOPIC"), false)
+                .usingSchemaRegistry(System.getProperty("SCHEMA_REGISTRY_URL"));
+
         Dataset<Row> df = spark
                 .readStream()
                 .format("kafka")
@@ -22,6 +35,8 @@ public class JiraIssueToOslcChangeRequest {
                 .option("startingOffsets", "earliest")
                 .option("kafka.group.id", System.getProperty("KAFKA_GROUP_ID"))
                 .load();
+
+        df = df.select(from_avro(col("value"), fromAvroConfig)).as("ConnectDefault");
 
         df.selectExpr("topic", "CAST(key AS STRING)", "CAST(value AS STRING)")
                 .writeStream()
