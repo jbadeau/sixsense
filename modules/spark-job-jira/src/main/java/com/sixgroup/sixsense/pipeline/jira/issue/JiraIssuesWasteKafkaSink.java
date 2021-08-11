e07d5dcebd8fdc8acb61faef5d065e4cb274f1f1
tomassatka <61469727+tomassatka@users.noreply.github.com>
2021-08-11 14:53:06 +0200

    Atlas (#7)
    atlas and amundsen

--- modules/spark-job-jira/src/main/java/com/sixgroup/sixsense/pipeline/jira/issue/JiraIssuesWasteKafkaSink.java
+++ modules/spark-job-jira/src/main/java/com/sixgroup/sixsense/pipeline/jira/issue/JiraIssuesWasteKafkaSink.java
@@ -21,7 +21,7 @@ public class JiraIssuesWasteKafkaSink {
                 .appName("JiraIssuesWasteKafkaSink")
                 .getOrCreate();
 
-        spark.sparkContext().hadoopConfiguration().set("fs.s3a.endpoint", System.getenv("MINIO_ENDPOINT"));
+        spark.sparkContext().hadoopConfiguration().set("fs.s3a.endpoint", "http://" + System.getenv("MINIO_SERVICE_HOST") + ":" + System.getenv("MINIO_SERVICE_PORT"));
         spark.sparkContext().hadoopConfiguration().set("fs.s3a.access.key", System.getenv("MINIO_ACCESS_KEY"));
         spark.sparkContext().hadoopConfiguration().set("fs.s3a.secret.key", System.getenv("MINIO_SECRET_KEY"));
         spark.sparkContext().hadoopConfiguration().set("fs.s3a.path.style.access", "true");
