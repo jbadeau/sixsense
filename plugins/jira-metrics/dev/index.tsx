82bce8d615c3b0873227bc4e2940a9396758805e
Jose Badeau <jose.badeau@gmail.com>
2021-08-31 23:10:56 +0200

    .

--- plugins/jira-metrics/dev/index.tsx
+++ plugins/jira-metrics/dev/index.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
-import { jiraMetricsPlugin, JiraMetricsPage } from '../src/plugin';
+import { jiraMetricsPlugin } from '../src/plugin';
 
 createDevApp()
   .registerPlugin(jiraMetricsPlugin)
-  .addPage({
-    element: <JiraMetricsPage />,
-    title: 'Root Page',
-    path: '/jira-metrics'
-  })
   .render();
